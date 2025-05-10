e561010c100c6c9c71548a9db02b88328ee81ad9
Haippp <haiperdesain@gmail.com>
2025-05-10 17:04:55 +0800

    Tahapan Ketiga - Memperbaiki menu ke 4

--- menu.cpp
+++ menu.cpp
@@ -4,64 +4,69 @@ using namespace std;
 
 void Info(){
   system("cls");
-  cout << "Indonesia Hebat, Poliban Juara" << endl;
+  cout << "\t===== Project UTS =====" << endl;
+  cout << "\n\nProgram ini berfungsi untuk melakukan \nInput pada array, Menampilkan \narray, serta menyorting/mengurutkannya" << endl;
+  cout << "\n\nDibuat oleh :" << endl;
+  cout << "\tNama \t: Hipni" << endl;
+  cout << "\tNim \t: C030324071" << endl;
+  cout << "\tKelas \t: TI - 2B" << endl;
   getch();
 }
 
 void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Informasi"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
+  system("cls");
+  cout<<"Aplikasi Tampilan Menu"<<"\n";   
+  cout<<"1. Menu Pertama"<<"\n";     
+  cout<<"2. Menu Kedua"<<"\n";
+  cout<<"3. Menu Ketiga"<<"\n";           
+  cout<<"4. Info"<<"\n";            
+  cout<<"5. Exit"<<"\n";           
+  cout<<"Masukan angka :";        
 }
 
 void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+  system("cls");
+  cout<<"hallo saya menu "<<pesan;
+  getch();
 }
 
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
+  char pl;
+  do
   {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    Info();
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
+      dMenu();
+      pl=getch();
+    switch (pl)
+    {
+    case '1':
+      /* code */
+      mPertama("pertama");
+      break;
+    case '2':
+      mPertama("ke- dua");
+      /* code */ 
+      break;  
+    case '3':
+      mPertama("ke- tiga");
+      /* code */
+      break;  
+    case '4':
+      Info();
+      /* code */
+      break;  
+    case '5':
+      /* code */
+      break;
+    
+    default:
+      system("cls");
+      cout<<"Pilihan Tidak Tersedia";
+      getch();
+      break;
+    }
 
 
-} while (pl!='5');
-  return 0;
+  } while (pl!='5');
+    return 0;
 }
